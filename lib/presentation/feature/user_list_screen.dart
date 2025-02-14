426a560f3bf34001e6fccf6cd12b5944a14682dd
akshada-dev10 <akshada.lawate@neosoftmail.com>
2025-02-14 11:28:17 +0530

    code reformat

--- lib/presentation/feature/user_list_screen.dart
+++ lib/presentation/feature/user_list_screen.dart
@@ -28,7 +28,7 @@ class _UserListScreenState extends ConsumerState<UserListScreen> {
   void _onScroll() {
     final userNotifier = ref.read(userProvider);
     if (_scrollController.position.pixels >=
-        _scrollController.position.maxScrollExtent - 100 &&
+            _scrollController.position.maxScrollExtent - 100 &&
         !userNotifier.isFetchingMore &&
         userNotifier.hasMoreData) {
       userNotifier.loadMore();
@@ -40,7 +40,7 @@ class _UserListScreenState extends ConsumerState<UserListScreen> {
     final userNotifier = ref.watch(userProvider);
     final List<User> users = userNotifier.users
         .where((user) =>
-        user.firstName!.toLowerCase().contains(searchQuery.toLowerCase()))
+            user.firstName!.toLowerCase().contains(searchQuery.toLowerCase()))
         .toList();
 
     return Scaffold(
@@ -50,92 +50,91 @@ class _UserListScreenState extends ConsumerState<UserListScreen> {
       body: users.isEmpty && userNotifier.isFetchingMore
           ? const Center(child: CircularProgressIndicator())
           : RefreshIndicator(
-        onRefresh: () async{
-          await ref.read(userProvider).fetchUsers(reset: true);
-        },
-            child: SingleChildScrollView(
-                    controller: _scrollController,
-                    child: Column(
-                              children: [
-                                Padding(
-                                  padding: const EdgeInsets.all(8.0),
-                                  child: TextField(
-                                    controller: searchController,
-                                    onChanged: (value) {
-                    setState(() {
-                      searchQuery = value;
-                    });
-                                    },
-                                    decoration: InputDecoration(
-                    prefixIcon: const Icon(Icons.search),
-                    hintText: 'Search...',
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(30),
-                      borderSide: BorderSide.none,
-                    ),
-                    filled: true,
-                    fillColor: Colors.grey[300],
-                    contentPadding: const EdgeInsets.symmetric(
-                        vertical: 12, horizontal: 16),
-                                    ),
-                                  ),
-                                ),
-                                ListView.builder(
-                                  physics: const NeverScrollableScrollPhysics(),
-                                  shrinkWrap: true,
-                                  itemCount:
-                                  users.length + (userNotifier.isFetchingMore ? 1 : 0),
-                                  itemBuilder: (context, index) {
-                                    if (index < users.length) {
-                    final user = users[index];
-                    return Card(
-                      elevation: 3,
-                      margin: const EdgeInsets.symmetric(
-                          horizontal: 12, vertical: 6),
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(12),
+              onRefresh: () async {
+                await ref.read(userProvider).fetchUsers(reset: true);
+              },
+              child: SingleChildScrollView(
+                controller: _scrollController,
+                child: Column(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: TextField(
+                        controller: searchController,
+                        onChanged: (value) {
+                          setState(() {
+                            searchQuery = value;
+                          });
+                        },
+                        decoration: InputDecoration(
+                          prefixIcon: const Icon(Icons.search),
+                          hintText: 'Search...',
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(30),
+                            borderSide: BorderSide.none,
+                          ),
+                          filled: true,
+                          fillColor: Colors.grey[300],
+                          contentPadding: const EdgeInsets.symmetric(
+                              vertical: 12, horizontal: 16),
+                        ),
                       ),
-                      child: ListTile(
-                        onTap: (){
-                          context.pushNamed(
-                            'userDetails',
-                            pathParameters: {
-                              'firstName': user.firstName!,
-                              'lastName': user.lastName!,
-                              'imageUrl': user.profilePicture!,
-                              'email' : user.email!
-                            },
-                          );
-                       },
-                                    contentPadding: const EdgeInsets.symmetric(
-                                    horizontal: 16, vertical: 8),
-                                    leading: CircleAvatar(
-                                    radius: 25,
-                                    backgroundImage: NetworkImage(
-                                    user.profilePicture ??
-                                    "https://i.pravatar.cc/150?img=${index + 1}",
-                                    ),
-                                    ),
-                                    title: Text(
-                                    '${user.firstName} ${user.lastName}',
-                                    style: const TextStyle(
-                                    fontWeight: FontWeight.bold, fontSize: 16),
-                                    ),
-                                    ),
-                                    );
-                                    }
-                                    else {
-                    return const Padding(
-                      padding: EdgeInsets.all(10),
-                      child: Center(child: CircularProgressIndicator()),
-                    );
-                                    }
-                                  },
-                                ),
-                              ],
                     ),
-                  ),
-          ),
+                    ListView.builder(
+                      physics: const NeverScrollableScrollPhysics(),
+                      shrinkWrap: true,
+                      itemCount:
+                          users.length + (userNotifier.isFetchingMore ? 1 : 0),
+                      itemBuilder: (context, index) {
+                        if (index < users.length) {
+                          final user = users[index];
+                          return Card(
+                            elevation: 3,
+                            margin: const EdgeInsets.symmetric(
+                                horizontal: 12, vertical: 6),
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(12),
+                            ),
+                            child: ListTile(
+                              onTap: () {
+                                context.pushNamed(
+                                  'userDetails',
+                                  pathParameters: {
+                                    'firstName': user.firstName!,
+                                    'lastName': user.lastName!,
+                                    'imageUrl': user.profilePicture!,
+                                    'email': user.email!
+                                  },
+                                );
+                              },
+                              contentPadding: const EdgeInsets.symmetric(
+                                  horizontal: 16, vertical: 8),
+                              leading: CircleAvatar(
+                                radius: 25,
+                                backgroundImage: NetworkImage(
+                                  user.profilePicture ??
+                                      "https://i.pravatar.cc/150?img=${index + 1}",
+                                ),
+                              ),
+                              title: Text(
+                                '${user.firstName} ${user.lastName}',
+                                style: const TextStyle(
+                                    fontWeight: FontWeight.bold, fontSize: 16),
+                              ),
+                            ),
+                          );
+                        } else {
+                          return const Padding(
+                            padding: EdgeInsets.all(10),
+                            child: Center(child: CircularProgressIndicator()),
+                          );
+                        }
+                      },
+                    ),
+                  ],
+                ),
+              ),
+            ),
     );
   }
 
